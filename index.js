781ee2ed6a860d6590561ac199cefe3f3064c9ad
Yaniv <yanivnavon123@gmail.com>
2023-01-11 14:37:45 +0200

    added download link and zip system

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const formidable = require('formidable');
 const fs = require('fs');
+const uuid = require('uuid');
+const zipFolder = require('zip-folder');
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended:false}));
@@ -12,14 +14,45 @@ app.use(bodyParser.text());
 
 app.post('/uploader',(req,res)=>{
     let form = formidable.IncomingForm();
+    let id = uuid();
+    let dir = 'uploads/' + id;
+    fs.mkdirSync(dir);
     form.on('file', (field, file)=>{
         let oldPath = file.path;
-        let newPath = '/uploads/' + file.name;
-        fs.writeFileSync(__dirname + newPath, fs.readFileSync(oldPath));
+        let newPath = dir + "/" + file.name;
+        fs.writeFileSync(__dirname + '/' + newPath, fs.readFileSync(oldPath));
+    });
+    form.on('end',()=>{
+        res.end(getURL(req) + '/' + id);
     });
     form.parse(req);
+});
+
+app.get('/:id',(req,res)=>{
+    let id = req.params.id;
+    let folder = __dirname + '/uploads/' + id;
+    let files = fs.readdirSync(folder);
+    if (files.length > 1) {
+        let dest = __dirname + '/zip/' + id + ".zip";
+        if (fs.existsSync(dest)) {
+            res.sendFile(dest);
+        } else {
+            console.log("generating zip...");
+            zipFolder(folder,dest,(err)=>{
+                if (err) throw err;
+                res.sendFile(dest);
+            });
+        }
+    } else {
+        res.sendFile(folder + '/' + files[0]);
+    }
+    
 });
 
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}!`);
 });
+
+function getURL(req) {
+    return req.protocol + '://' + req.get('host');
+}
